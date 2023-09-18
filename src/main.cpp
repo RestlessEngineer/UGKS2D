3e72cb4922a1e43dbf5b121f0c5bae831ffcffb8
RestlessEngineer <naz.vladislav@yandex.ru>
2023-09-18 16:46:26 +0400

    add parser

--- src/main.cpp
+++ src/main.cpp
@@ -4,99 +4,211 @@
 #include "solver.h"
 #include <Eigen/Dense>
 #include <iostream>
+#include <fstream>
+#include <argparse/argparse.hpp>
+#include <exception>
+#include <vector>
+#include <nlohmann/json.hpp>
 
+using json = nlohmann::json;
+
+argparse::ArgumentParser arguments("UGKS");
+
+void init_arguments_parser(argparse::ArgumentParser &arguments)
+{
+
+    arguments.add_argument("-i", "--init")
+        .action([](const std::string &value) { // read a JSON file
+            std::ifstream stream(value);
+            json init;
+            stream >> init;
+            stream.close();
+            return init;
+        })
+        .required()
+        .help("Init file in .json format. It sets parameters for all programm");
+
+    arguments.add_argument("-o", "--output")
+        .required()
+        .help("Output file for results. All results will be write there in a .plt format");
+
+    arguments.add_argument("-r", "--output-rate")
+        .default_value(-1)
+        .help("Rate defines how often output file will be written");
+
+    
+}
 
 int main(int argc, char *argv[]){
 
-    const double residual = 1e-5;
-    const double CFL = 0.8; // Courant–Friedrichs–Lewy number
+    init_arguments_parser(arguments);
 
-    const double kn = 0.1;        // 0.075 Knudsen number in reference state
-    const double alpha_ref = 1.0; // coefficient in HS model
-    const double omega_ref = 0.5; // coefficient in HS model
+    try
+    {
+        arguments.parse_args(argc, argv);
 
-    ugks::physic_val phys;
-    phys.DOF = 1;
-    phys.gamma = ugks::tools::get_gamma(phys.DOF); //ratio of specific heat
-    phys.Pr = 2.0/3.0;
-    phys.omega = 0.72;
-    phys.mu_ref = ugks::tools::get_mu(kn, alpha_ref, omega_ref); //reference viscosity coefficient
+        json init_params = arguments.get<json>("--init")[0];
+        auto output_file = arguments.get<std::string>("--output");
+        auto rate = arguments.get<int>("--output-rate");
 
-    //create solver
-    ugks::solver ugks_solver(45,45, phys, ugks::precision::SECOND_ORDER, CFL);
-    Eigen::Rotation2D<double> rot(-30./180.*M_PI);
-    Eigen::Vector2d p1 = {0.,0.};
-    Eigen::Vector2d p2 = {0.,1.};
-    Eigen::Vector2d p3 = {1.,1.};
-    Eigen::Vector2d p4 = {1.,0.};
-    Eigen::Vector2d uvec = {0.15,0.};
+        // auto params = json::parse(init_params);
 
-    Eigen::Vector2d rot_p1 = rot*p1;
-    Eigen::Vector2d rot_p2 = rot*p2;
-    Eigen::Vector2d rot_p3 = rot*p3;
-    Eigen::Vector2d rot_p4 = rot*p4;
-    Eigen::Vector2d rotu = rot*uvec;
+        // calculation params
+        double CFL = init_params["Calculation"]["CFL"]; // Courant–Friedrichs–Lewy number
+        double residual = init_params["Calculation"]["residual"];
+        ugks::precision prec = init_params["Calculation"]["precision"] == 1 ? ugks::precision::FIRST_ORDER : ugks::precision::SECOND_ORDER;
 
+        // physic parameters
+        double kn = init_params["Physic"]["Knudsen"];         // Knudsen number in reference state
+        double alpha_ref = init_params["Physic"]["AlphaRef"]; // coefficient in HS model
+        double omega_ref = init_params["Physic"]["OmegaRef"]; // coefficient in HS model
 
-    ugks::point pp1 = {rot_p1[0], rot_p1[1]};
-    ugks::point pp2 = {rot_p2[0], rot_p2[1]};
-    ugks::point pp3 = {rot_p3[0], rot_p3[1]};
-    ugks::point pp4 = {rot_p4[0], rot_p4[1]};
+        ugks::physic_val phys;
+        phys.DOF = init_params["Physic"]["DOF"];
+        phys.Pr = init_params["Physic"]["Prantl"];
+        phys.omega = init_params["Physic"]["Omega"];
+        phys.gamma = ugks::tools::get_gamma(phys.DOF);               // ratio of specific heat
+        phys.mu_ref = ugks::tools::get_mu(kn, alpha_ref, omega_ref); // reference viscosity coefficient
 
-    //set geometry area. box
-    ugks_solver.set_geometry({pp2, pp3}, {pp1, pp4});
+        // geometry
+        auto gsize = init_params["Geometry"]["size"];
+        auto RawUpVal = init_params["Geometry"]["UpWall"];
+        auto RawDownVal = init_params["Geometry"]["DownWall"];
+        std::vector<ugks::point> upWall, downWall;
+        auto insert_point = [](auto && x) -> ugks::point
+        { return {x[0], x[1]};};
 
-    //set velocity space param
-    ugks::vel_space_param param;
-    // largest discrete velocity
-    param.max_u = 3;
-    param.max_v = 3;
-    // smallest discrete velocity
-    param.min_u = -3;
-    param.min_v = -3;
-    // number of velocity points
-    param.num_u = 25; 
-    param.num_v = 25;
+        std::transform(RawUpVal.cbegin(), RawUpVal.cend(),
+                       std::back_inserter(upWall), insert_point);
 
-    ugks_solver.set_velocity_space(param, ugks::integration::NEWTON_COTES);
+        std::transform(RawDownVal.cbegin(), RawDownVal.cend(),
+                       std::back_inserter(downWall), insert_point);
 
-    // set boundary condition (density,u-velocity,v-velocity,lambda=1/temperature)
-    ugks_solver.set_boundary(ugks::boundary_side::LEFT, {1.0, 0.0, 0.0, 1.0}, ugks::boundary_type::WALL);
-    ugks_solver.set_boundary(ugks::boundary_side::RIGHT, {1.0, 0.0, 0.0, 1.0}, ugks::boundary_type::WALL);
-    ugks_solver.set_boundary(ugks::boundary_side::UP, {1.0, rotu[0], rotu[1], 1.0}, ugks::boundary_type::WALL); 
-    ugks_solver.set_boundary(ugks::boundary_side::DOWN, {1.0, 0.0, 0.0, 1.0}, ugks::boundary_type::WALL);
+        ugks::solver ugks_solver(gsize[0], gsize[1], phys, prec, CFL);
+        ugks_solver.set_geometry(upWall, downWall);
 
-    // initial condition (density,u-velocity,v-velocity,lambda=1/temperature)
-    ugks_solver.set_flow_field({1.0, 0.0, 0.0, 1.0});
-    ugks_solver.write_results();
+        // velocity space
+        // set velocity space param
+        ugks::vel_space_param param;
+        // largest discrete velocity
+        auto vsize = init_params["VelocitySpace"]["size"];
+        auto urange = init_params["VelocitySpace"]["XSpaceRange"];
+        auto vrange = init_params["VelocitySpace"]["YSpaceRange"];
 
-    while( true ){
+        std::string integ_name = init_params["VelocitySpace"]["IntegrationMethod"];
 
-        auto sim = ugks_solver.solve();
-
-        auto max_res = std::max_element(sim.res.begin(), sim.res.end());
-
-        //check is nan
-        bool is_nan = false;
-        std::for_each(sim.res.begin(), sim.res.end(), [&is_nan](auto val){ is_nan = is_nan || std::isnan(val);});
-        if(is_nan){
-            std::cout << "nan was detected; res: "<< sim.res.transpose() << std::endl;
-            return -1;
+        ugks::integration integ;
+        if (integ_name == "NEWTON_COTES")
+        {
+            integ = ugks::integration::NEWTON_COTES;
+        }
+        else if (integ_name == "GAUSS")
+        {
+            integ = ugks::integration::GAUSS;
+        }
+        else
+        {
+            throw std::invalid_argument("wrong integration type: " + integ_name);
         }
 
-        //check if exit
-        if (*max_res < residual)
-            break;
+        param.max_u = urange[1];
+        param.max_v = vrange[1];
+        // smallest discrete velocity
+        param.min_u = urange[0];
+        param.min_v = vrange[0];
+        // number of velocity points
+        param.num_u = vsize[1];
+        param.num_v = vsize[0];
 
-        if( sim.cnt_iter%10 == 0){
-            std::cout << "iter: "<< sim.cnt_iter <<
-             " sitime: "<<sim.sitime << 
-            " dt: "<< sim.dt << std::endl;
-            std::cout << "res: "<< sim.res.transpose() << std::endl;
+        ugks_solver.set_velocity_space(param, integ);
+
+        // boundary
+        auto boundary_init = [&init_params](const std::string& side) -> std::tuple<ugks::boundary_type, Eigen::Array4d>
+        {
+            std::string stype = init_params[side]["Type"];
+            ugks::boundary_type type;
+            if(stype == "WALL")
+                type = ugks::boundary_type::WALL;
+            else if(stype == "INPUT")
+                type = ugks::boundary_type::INPUT;
+            else if(stype == "MIRROR")
+                type = ugks::boundary_type::MIRROR;
+            else if(stype == "OUTPUT")
+                type = ugks::boundary_type::OUTPUT;
+            else
+                throw std::invalid_argument("wrong boundary type: " + stype);
+            
+
+            auto in_bound = init_params[side]["Init"]; 
+            Eigen::Array4d bound;
+            std::copy(in_bound.begin(), in_bound.end(), bound.begin());
+            return {type, bound};
+        };
+
+        auto [ltype, lbound] = boundary_init("LEFT");
+        auto [rtype, rbound] = boundary_init("RIGHT");
+        auto [utype, ubound] = boundary_init("UP");
+        auto [dtype, dbound] = boundary_init("DOWN");
+
+        ugks_solver.set_boundary(ugks::boundary_side::LEFT,  lbound, ltype);
+        ugks_solver.set_boundary(ugks::boundary_side::RIGHT, rbound, rtype);
+        ugks_solver.set_boundary(ugks::boundary_side::UP,    ubound, utype);
+        ugks_solver.set_boundary(ugks::boundary_side::DOWN,  dbound, dtype);
+
+        // initial condition (density,u-velocity,v-velocity,lambda=1/temperature)
+        auto in_field = init_params["FlowField"]; 
+        Eigen::Array4d field;
+        std::copy(in_field.begin(), in_field.end(), field.begin());
+        ugks_solver.set_flow_field(field);
+
+        while( true ){
+
+            auto sim = ugks_solver.solve();
+            
+            auto max_res = std::max_element(sim.res.begin(), sim.res.end());
+
+            //check is nan
+            bool is_nan = std::accumulate(sim.res.begin(), sim.res.end(), false,
+                                        [](auto &&val1, auto &&val2)
+                                        {return std::isnan(val1) || std::isnan(val2); });
+            if(is_nan){
+                std::cout << "nan was detected; res: "<< sim.res.transpose() << std::endl;
+                return -1;
+            }
+
+            //check if exit
+            if (*max_res < residual)
+                break;
+
+            if( sim.cnt_iter%10 == 0){
+                std::cout << "iter: "<< sim.cnt_iter <<
+                " sitime: "<<sim.sitime << 
+                " dt: "<< sim.dt << std::endl;
+                std::cout << "res: "<< sim.res.transpose() << std::endl;
+            }
         }
+
+        ugks_solver.write_results();
+
+        // std::cout << "init: " << init_params.dump(4) << std::endl;
+        // std::cout << "output: " << output_file << std::endl;
+        // std::cout << "rate: " << rate << std::endl;
+    }
+    catch (const std::runtime_error &err)
+    {
+        std::cerr << err.what() << std::endl;
+        std::cerr << arguments;
+        std::exit(1);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "Error reading or parsing JSON: " << e.what() << std::endl;
+        return 1;
     }
 
-    ugks_solver.write_results();
+
+
+
 
     return 0;
 
